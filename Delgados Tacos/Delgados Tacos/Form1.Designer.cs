79005473628ea0dbdf44b0b251d21cd197fbc1cc
jh145216 <31322578+jh145216@users.noreply.github.com>
2018-02-07 13:52:44 -0600

    update Delgados Tacos

---
+++ Delgados Tacos/Delgados Tacos/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace Delgados_Tacos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblExtraTotal = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblMainTotal = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.radNo = new System.Windows.Forms.RadioButton();
+            this.radYes = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtEnterNameHere = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radCheeseQuesadilla = new System.Windows.Forms.RadioButton();
+            this.radPop = new System.Windows.Forms.RadioButton();
+            this.radFajita = new System.Windows.Forms.RadioButton();
+            this.radReFriedBeans = new System.Windows.Forms.RadioButton();
+            this.radBurritos = new System.Windows.Forms.RadioButton();
+            this.radTacos = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblExtraTotal
+            // 
+            this.lblExtraTotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblExtraTotal.Location = new System.Drawing.Point(331, 384);
+            this.lblExtraTotal.Name = "lblExtraTotal";
+            this.lblExtraTotal.Size = new System.Drawing.Size(102, 46);
+            this.lblExtraTotal.TabIndex = 41;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(341, 356);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 17);
+            this.label6.TabIndex = 40;
+            this.label6.Text = "Extra Total";
+            // 
+            // lblMainTotal
+            // 
+            this.lblMainTotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblMainTotal.Location = new System.Drawing.Point(79, 384);
+            this.lblMainTotal.Name = "lblMainTotal";
+            this.lblMainTotal.Size = new System.Drawing.Size(111, 46);
+            this.lblMainTotal.TabIndex = 39;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(90, 356);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 17);
+            this.label5.TabIndex = 38;
+            this.label5.Text = "Main Total";
+            // 
+            // radNo
+            // 
+            this.radNo.AutoSize = true;
+            this.radNo.Location = new System.Drawing.Point(425, 210);
+            this.radNo.Name = "radNo";
+            this.radNo.Size = new System.Drawing.Size(39, 17);
+            this.radNo.TabIndex = 37;
+            this.radNo.TabStop = true;
+            this.radNo.Text = "No";
+            this.radNo.UseVisualStyleBackColor = true;
+            // 
+            // radYes
+            // 
+            this.radYes.AutoSize = true;
+            this.radYes.Location = new System.Drawing.Point(316, 210);
+            this.radYes.Name = "radYes";
+            this.radYes.Size = new System.Drawing.Size(43, 17);
+            this.radYes.TabIndex = 36;
+            this.radYes.TabStop = true;
+            this.radYes.Text = "Yes";
+            this.radYes.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(272, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(252, 17);
+            this.label4.TabIndex = 35;
+            this.label4.Text = "Would you like to order anything else";
+            // 
+            // txtEnterNameHere
+            // 
+            this.txtEnterNameHere.Location = new System.Drawing.Point(316, 122);
+            this.txtEnterNameHere.Name = "txtEnterNameHere";
+            this.txtEnterNameHere.Size = new System.Drawing.Size(148, 20);
+            this.txtEnterNameHere.TabIndex = 34;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(313, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(151, 17);
+            this.label3.TabIndex = 33;
+            this.label3.Text = "Enter your name here";
+            // 
+            // radCheeseQuesadilla
+            // 
+            this.radCheeseQuesadilla.AutoSize = true;
+            this.radCheeseQuesadilla.Location = new System.Drawing.Point(79, 302);
+            this.radCheeseQuesadilla.Name = "radCheeseQuesadilla";
+            this.radCheeseQuesadilla.Size = new System.Drawing.Size(143, 17);
+            this.radCheeseQuesadilla.TabIndex = 32;
+            this.radCheeseQuesadilla.TabStop = true;
+            this.radCheeseQuesadilla.Text = "Cheese Quesadilla $1.75";
+            this.radCheeseQuesadilla.UseVisualStyleBackColor = true;
+            // 
+            // radPop
+            // 
+            this.radPop.AutoSize = true;
+            this.radPop.Location = new System.Drawing.Point(79, 270);
+            this.radPop.Name = "radPop";
+            this.radPop.Size = new System.Drawing.Size(74, 17);
+            this.radPop.TabIndex = 31;
+            this.radPop.TabStop = true;
+            this.radPop.Text = "Pop $1.00";
+            this.radPop.UseVisualStyleBackColor = true;
+            // 
+            // radFajita
+            // 
+            this.radFajita.AutoSize = true;
+            this.radFajita.Location = new System.Drawing.Point(79, 234);
+            this.radFajita.Name = "radFajita";
+            this.radFajita.Size = new System.Drawing.Size(80, 17);
+            this.radFajita.TabIndex = 30;
+            this.radFajita.TabStop = true;
+            this.radFajita.Text = "Fajita $1.50";
+            this.radFajita.UseVisualStyleBackColor = true;
+            // 
+            // radReFriedBeans
+            // 
+            this.radReFriedBeans.AutoSize = true;
+            this.radReFriedBeans.Location = new System.Drawing.Point(79, 199);
+            this.radReFriedBeans.Name = "radReFriedBeans";
+            this.radReFriedBeans.Size = new System.Drawing.Size(125, 17);
+            this.radReFriedBeans.TabIndex = 29;
+            this.radReFriedBeans.TabStop = true;
+            this.radReFriedBeans.Text = "ReFried Beans $0.50";
+            this.radReFriedBeans.UseVisualStyleBackColor = true;
+            // 
+            // radBurritos
+            // 
+            this.radBurritos.AutoSize = true;
+            this.radBurritos.Location = new System.Drawing.Point(79, 163);
+            this.radBurritos.Name = "radBurritos";
+            this.radBurritos.Size = new System.Drawing.Size(90, 17);
+            this.radBurritos.TabIndex = 28;
+            this.radBurritos.TabStop = true;
+            this.radBurritos.Text = "Burritos $1.99";
+            this.radBurritos.UseVisualStyleBackColor = true;
+            // 
+            // radTacos
+            // 
+            this.radTacos.AutoSize = true;
+            this.radTacos.Location = new System.Drawing.Point(79, 125);
+            this.radTacos.Name = "radTacos";
+            this.radTacos.Size = new System.Drawing.Size(85, 17);
+            this.radTacos.TabIndex = 27;
+            this.radTacos.TabStop = true;
+            this.radTacos.Text = "Tacos $0.99";
+            this.radTacos.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(33, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(202, 17);
+            this.label2.TabIndex = 26;
+            this.label2.Text = "What would you want to order";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(133, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(258, 24);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "Welcome to Delgados Tacos";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(543, 458);
+            this.Controls.Add(this.lblExtraTotal);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblMainTotal);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.radNo);
+            this.Controls.Add(this.radYes);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtEnterNameHere);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.radCheeseQuesadilla);
+            this.Controls.Add(this.radPop);
+            this.Controls.Add(this.radFajita);
+            this.Controls.Add(this.radReFriedBeans);
+            this.Controls.Add(this.radBurritos);
+            this.Controls.Add(this.radTacos);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Delgados Tacos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblExtraTotal;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblMainTotal;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton radNo;
+        private System.Windows.Forms.RadioButton radYes;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtEnterNameHere;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radCheeseQuesadilla;
+        private System.Windows.Forms.RadioButton radPop;
+        private System.Windows.Forms.RadioButton radFajita;
+        private System.Windows.Forms.RadioButton radReFriedBeans;
+        private System.Windows.Forms.RadioButton radBurritos;
+        private System.Windows.Forms.RadioButton radTacos;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
