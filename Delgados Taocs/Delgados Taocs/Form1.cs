0bb5897033ab4d6609dcfcc97b6cb0f7957681e8
jh145216 <31322578+jh145216@users.noreply.github.com>
2018-02-08 14:42:33 -0600

    update DT

--- Delgados Taocs/Delgados Taocs/Form1.cs
+++ Delgados Taocs/Delgados Taocs/Form1.cs
@@ -19,14 +19,53 @@ namespace Delgados_Taocs
 
         private void btnOrder_Click(object sender, EventArgs e)
         {
-            int tacos;
+            /*int tacos;
             int burritos;
             int refriedbeans;
             int fajita;
             int pop;
             int cheesequesadilla;
             int yes;
-            int no;
+            int no;*/
+
+
+            if (radTacos.Checked == true)
+            {
+                lblMainTotal.Text = "$0.99";
+            }
+
+            if (radBurritos.Checked == true)
+            {
+                lblMainTotal.Text = "$1.99";
+            }
+
+            if (radReFriedBeans.Checked == true)
+            {
+                lblMainTotal.Text = "$0.50";
+            }
+
+            if (radFajita.Checked == true)
+            {
+                lblMainTotal.Text = "$1.50";
+            }
+
+            if (radPop.Checked == true)
+            {
+                lblMainTotal.Text = "$1.00";
+            }
+
+            if (radCheeseQuesadilla.Checked == true)
+            {
+                lblMainTotal.Text = "$1.75";
+            }
+
+            /*radTacos.Text = (radTacos.Text);
+            radBurritos.Text = (radBurritos.Text);
+            radReFriedBeans.Text = (radReFriedBeans.Text);
+            radFajita.Text = (radFajita.Text);
+            radPop.Text = (radPop.Text);
+            int cheesequesaddilla = (radCheeseQuesadilla.Text);
+            Console.WriteLine(radCheeseQuesadilla.Text);*/
 
         }
     }
