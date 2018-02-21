fe2bb9b2b99b31ce0068a4b5510f60739612462c
jh145216 <31322578+jh145216@users.noreply.github.com>
2018-02-21 13:08:15 -0600

    update FBW

--- Fizz Buzz Woof1/Fizz Buzz Woof1/Program.cs
+++ Fizz Buzz Woof1/Fizz Buzz Woof1/Program.cs
@@ -10,7 +10,22 @@ namespace Fizz_Buzz_Woof
     {
         static void Main(string[] args)
         {
+            // step 1 : Print number from 1 to 100
+            for (int j = 1; j <= 100; j++)
+            {
+                string results = "";
+                // Step 2 :- Divisible by 3 print Fizz
+                if (j % 3 == 0) results = "Fizz ";
+                // Step 3 :- Divisible by 5 print Buzz
+                if (j % 5 == 0) results = results + "Buzz";
+                // Step 4 :- Divisible by 3 and 5 print FizzBuzz
 
+                // Step 5 :- Print the number as it is if not divisible ny 3 or 5
+                if (results.Length == 0) results = j.ToString();
+
+                Console.WriteLine(results);
+
+            }
         }
     }
 }
